8ead0d5a4dadc05a73a249de468734400dcf2b97
Mariano Venesio <mariano.venesio@tiarg.com>
2019-09-02 16:54:13 -0300

    add throw coin logic

--- src/app/pages/coin/throw-coin/throw-coin.component.ts
+++ src/app/pages/coin/throw-coin/throw-coin.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type ExperimentResult = {
+  name: string, total: number
+};
 
 @Component({
   selector: 'ngx-throw-coin',
   templateUrl: './throw-coin.component.html',
 })
 export class ThrowCoinComponent {
+  @Input() numberOfExperiments: number = 2000;
+  @Input() resultOfExperiment: ExperimentResult[] = [];
+  @Input() omega: string[] = ['CARA', 'SECA'];
+  @Input() resultsLog: string[] = [];
+  public startExperiment() {
+    for (let i = 0 ; i < this.numberOfExperiments; i++) {
+      this.resultsLog.push(this.omega[Math.round(Math.random())]);
+    }
+    for (let i = 0 ; i < this.omega.length; i++) {
+      this.resultOfExperiment.push({name: this.omega[i], total: this.resultsLog.filter((result) => result === this.omega[i]).length});
+    }
+  }
 }
--- src/app/pages/coin/throw-coin/throw-coin.module.ts
+++ src/app/pages/coin/throw-coin/throw-coin.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule } from '@nebular/theme';
-
+import { NbCardModule, NbInputModule, NbButtonModule, NbListModule } from '@nebular/theme';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ThemeModule } from '../../../@theme/theme.module';
+
 import { ThrowCoinComponent } from './throw-coin.component';
 
 @NgModule({
   imports: [
     NbCardModule,
+    NbInputModule,
+    NbButtonModule,
     ThemeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbListModule,
   ],
   declarations: [
     ThrowCoinComponent,
