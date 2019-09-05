3581373d18d2797eb216bf0b13c893a3b00ad324
mariano <mavenesio@gmail.com>
2019-09-04 23:28:11 -0300

    Fix show results on throw/count until

--- src/app/pages/coin/throw-coin-until/throw-coin-until.component.ts
+++ src/app/pages/coin/throw-coin-until/throw-coin-until.component.ts
@@ -12,14 +12,14 @@ export class ThrowCoinUntilComponent {
   @Input() numberOfExperiments: number = 2000;
   @Input() resultOfExperiment: ExperimentResult[] = [];
   @Input() omega: string[] = ['CARA', 'SECA'];
-  @Input() resultsLog: any[];
+  @Input() resultsLog: any[] = [];
   private maxNumberOfExperiments: number = 2000;
   private selectedUntil: string;
   public loading: boolean = false;
 
 
   public startExperiment() {
-    if(this.selectedUntil === undefined || this.selectedUntil === null) return
+    if(this.selectedUntil === undefined || this.selectedUntil === null) return;
     this.loading = true;
     if (this.numberOfExperiments > this.maxNumberOfExperiments) this.maxNumberOfExperiments = this.maxNumberOfExperiments;
     this.resultsLog = [];
@@ -34,8 +34,37 @@ export class ThrowCoinUntilComponent {
       this.resultsLog.push(experiment);
     }
     this.loading = false;
+
+      this.resultOfExperiment = this.resultsLog
+      .map(result => {
+        return result.length;
+      })
+      .reduce(
+        (b,c)=>(
+          ( b[b.findIndex(d => d.result === c)]
+            || b[b.push({ result: c, total: 0 }) - 1])
+                .total++, b), [])
+      .sort((a,b) => (a.result > b.result) ? 1 : ((b.result > a.result) ? -1 : 0));
+
+
   }
   untilOnchange(event) {
     this.selectedUntil = event;
   }
+  /*
+  private occurrence (array) {
+    let result = {};
+    let res = []
+    if (array instanceof Array) { // Check if input is array.
+        array.forEach(function (v, i) {
+            if (!result[v]) { // Initial object property creation.
+                result[v] = [i]; // Create an array for that property.
+            } else { // Same occurrences found.
+                result[v].push(i); // Fill the array.
+            }
+        });
+    }
+    return result;
+}
+*/
 }
