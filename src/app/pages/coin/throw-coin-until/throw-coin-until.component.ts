9facf1ec239feda91b99e227c387d1d96f761d7d
mariano <mavenesio@gmail.com>
2019-09-07 02:47:29 -0300

    refactor throw coin until

--- src/app/pages/coin/throw-coin-until/throw-coin-until.component.ts
+++ src/app/pages/coin/throw-coin-until/throw-coin-until.component.ts
@@ -1,65 +1,41 @@
 import { Component, Input } from '@angular/core';
+import { ThrowCoinResult } from '../../../models/throw-coin-result';
+import { CoinService } from '../../../services/coin.service';
 
-type ExperimentResult = {
-  result: string,
-  total: number,
-};
 @Component({
   selector: 'ngx-throw-coin-until',
   templateUrl: './throw-coin-until.component.html',
 })
 export class ThrowCoinUntilComponent {
   @Input() numberOfExperiments: number = 2000;
-
-  public resultOfExperiment: ExperimentResult[] = [];
-  public omega: string[] = ['CARA', 'SECA'];
-  public resultsLog: any[] = [];
+  public resultOfExperiment: ThrowCoinResult[] = [];
   public loading: boolean = false;
   public flipped: boolean = false;
-  public data: any[] = [];
+  public selectedUntil: string;
+  public view = [600 , 400];
+  private maxNumberOfExperiments: number = 100000000;
 
-  private maxNumberOfExperiments: number = 1000000;
-  private selectedUntil: string;
+  constructor(private _coinService: CoinService) {
+    this.selectedUntil = 'CARA';
+  }
 
   public startExperiment() {
-    if (this.selectedUntil === undefined || this.selectedUntil === null) return;
     this.loading = true;
-    if (this.numberOfExperiments > this.maxNumberOfExperiments) this.numberOfExperiments = this.maxNumberOfExperiments;
-    this.resultsLog = [];
-    for (let i = 0 ; i < this.numberOfExperiments; i++) {
-      let experimentIsOver = false;
-      const experiment: string[] = [];
-      while (!experimentIsOver) {
-        const cointThrow = this.omega[Math.round(Math.random())];
-        experiment.push(cointThrow);
-        experimentIsOver = (cointThrow === this.selectedUntil);
-      }
-      this.resultsLog.push(experiment);
-    }
+    this.cleanResults();
+    this.validate();
+    this.resultOfExperiment = this._coinService.throwCointUntil(this.numberOfExperiments, this.selectedUntil);
     this.loading = false;
-
-      this.resultOfExperiment = this.resultsLog
-      .map(result => {
-        return result.length;
-      })
-      .reduce(
-        (b,c)=>(
-          ( b[b.findIndex(d => d.result === c)]
-            || b[b.push({ result: c, total: 0 }) - 1])
-                .total++, b), [])
-      .sort((a,b) => (a.result > b.result) ? 1 : ((b.result > a.result) ? -1 : 0));
-      this.data = [];
-      this.data = this.resultOfExperiment.map(experiment => {
-        return {
-          name: experiment.result,
-          value: experiment.total,
-        };
-      });
   }
-  untilOnchange(event) {
+  public validate(): void {
+    if (this.numberOfExperiments > this.maxNumberOfExperiments) this.numberOfExperiments = this.maxNumberOfExperiments;
+  }
+  public untilOnchange( event): void {
     this.selectedUntil = event;
   }
-  public toggleView() {
+  public toggleView(): void {
     this.flipped = !this.flipped;
+  }
+  public cleanResults(): void {
+    this.resultOfExperiment = [];
   }
 }
