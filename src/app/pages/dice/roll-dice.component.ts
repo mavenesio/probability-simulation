7c7e869a61fe6e05616a5401a733c675365cf58f
mariano <mavenesio@gmail.com>
2019-09-08 20:49:01 -0300

    add roll dice logic to roll dice view

--- src/app/pages/dice/roll-dice.component.ts
+++ src/app/pages/dice/roll-dice.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { DiceService } from '../../services/dice.service';
 
 @Component({
   selector: 'ngx-roll-dice',
@@ -10,26 +11,35 @@ export class RollDiceComponent {
   @Input() numberOfFaces: number;
   @Input() result: number = 7;
 
-  public conditions = ['Igual', 'Mayor', 'Mayor o Igual', 'Menor', 'Menor o Igual', 'Par', 'Impar'];
+  public view = [500, 300];
   public selectedCondition: string;
   public selectedConditionHasResult: boolean;
   public loading = false;
   public flipped: boolean = false;
   public maxResults: number[] = [];
+  public conditions = [];
 
-constructor() {
-  this.selectedCondition = this.conditions[0];
-  this.selectedConditionHasResult = true;
-  this.numberOfDices = 1;
-  this.numberOfFaces = 6;
-}
+  public resultOfExperiment: any[] = [];
+
+  constructor(private _diceService: DiceService) {
+    this.conditions = _diceService.getConditions();
+    this.selectedCondition = this.conditions[0];
+    this.selectedConditionHasResult = true;
+    this.numberOfDices = 1;
+    this.numberOfFaces = 6;
+  }
 
   public startExperiment() {
-    console.log('start experiment');
+    const results = this._diceService.getResultOfDiceExperiment(4, 6, this.conditions[7], 6, 100);
+    this.resultOfExperiment = this._diceService.getGroupedResults(results);
   }
   public conditionOnchange() {
-    this.selectedConditionHasResult = (this.selectedCondition !== 'Par' && this.selectedCondition !== 'Impar' );
+    this.selectedConditionHasResult = (this.selectedCondition !== this.conditions[5] &&
+                                       this.selectedCondition !== this.conditions[6] &&
+                                       this.selectedCondition !== this.conditions[7]);
   }
+
+
   public NumberOfChange() {
     this.maxResults = [];
     const max: number = this.numberOfDices * this.numberOfFaces;
