141c1af5ea04bd4e45102ec683220be6f10a434c
Mariano Venesio <mariano.venesio@tiarg.com>
2019-09-17 18:02:50 -0300

    refactor coin service

--- src/app/services/coin.service.ts
+++ src/app/services/coin.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ThrowCoinResult } from '../models/throw-coin-result';
+import { FlipCoinResult } from '../models/flip-coin-result';
 
 @Injectable({
   providedIn: 'root',
@@ -9,47 +9,53 @@ export class CoinService {
 
   constructor() { }
 
-  public throwCoin(repetitions: number, probability: number): ThrowCoinResult[] {
-    // experiment
-    const resultsLog = [];
-    for (let i = 0 ; i < repetitions; i++) {
-      resultsLog.push(this.getThrownOutCoin(probability));
-    }
-    // parse experiment
-    const resultOfExperiment = [];
-    for (let i = 0; i < this.omega.length; i++) {
-      const total = resultsLog.filter((result) => result === this.omega[i]).length;
-      resultOfExperiment.push({name: this.omega[0], value: total, probability: (total / repetitions).toFixed(6)});
-
-    }
-    return resultOfExperiment;
+  public flipCoinExperiment(repetitions: number, probability: number): FlipCoinResult[] {
+    const resultsLog = this.doFlipCoinExperiment(repetitions, probability);
+    return this.parseFlipCoinExperiment(resultsLog, repetitions);
   }
 
-
-  public throwCointUntil(repetitions: number, until: string, probability: number): ThrowCoinResult[] {
-    // experiment
-    const resultsLog = [];
-    for (let i = 0 ; i < repetitions; i++) {
-      let experimentIsOver = false;
-      let count: number = 0;
-      while (!experimentIsOver) {
-        const cointThrow = this.getThrownOutCoin(probability);
-        count++;
-        experimentIsOver = (cointThrow === until);
-      }
-      resultsLog.push(count);
-    }
-    // parse experiment
-    return this.getResults(resultsLog, repetitions);
+  public flipCointUntilExperiment(repetitions: number, until: string, probability: number, numberOfTimes): FlipCoinResult[] {
+    const resultsLog = this.doFlipCoinUntilExperiment(repetitions, until, probability, numberOfTimes);
+    return this.parseFlioCoinUntilExperiment(resultsLog, repetitions);
   }
 
   // private functions
-  private getThrownOutCoin(probability: number): string {
+
+  private flipnCoin(probability: number): string {
     const random = (Math.random() > (probability)) ? 1 : 0;
     return this.omega[random];
   }
 
-  private getResults(resultsLog: number[], repetitions: number ): ThrowCoinResult[] {
+  private doFlipCoinExperiment(repetitions: number, probability: number) {
+    const resultsLog = [];
+    for (let i = 0 ; i < repetitions; i++) {
+      resultsLog.push(this.flipnCoin(probability));
+    }
+    return resultsLog;
+  }
+
+  private parseFlipCoinExperiment(resultsLog: string[], repetitions: number) {
+    const totalHead = resultsLog.filter((result) => result === this.omega[0]).length;
+    const totalTail = resultsLog.filter((result) => result === this.omega[1]).length;
+    return [{name: this.omega[0], value: totalHead, probability: (totalHead / repetitions).toFixed(6)},
+            {name: this.omega[1], value: totalTail, probability: (totalTail / repetitions).toFixed(6)}];
+  }
+
+  private doFlipCoinUntilExperiment(repetitions: number, until: string, probability: number, numberOfTimes) {
+    const resultsLog = [];
+    for (let i = 0 ; i < repetitions; i++) {
+      let sightings = 0;
+      let count: number = 0;
+      while (sightings < numberOfTimes) {
+        if (this.flipnCoin(probability) === until) sightings++;
+        count++;
+      }
+      resultsLog.push(count);
+    }
+    return resultsLog;
+  }
+
+  private parseFlioCoinUntilExperiment(resultsLog: number[], repetitions: number ): FlipCoinResult[] {
     const experimentResults =  resultsLog
             .reduce( (a, b) => (
             ( a[a.findIndex(d => d.name === b)] ||
@@ -63,7 +69,7 @@ export class CoinService {
     });
   }
 
-  private sortExperimentResults(results: ThrowCoinResult[]): ThrowCoinResult[] {
+  private sortExperimentResults(results: FlipCoinResult[]): FlipCoinResult[] {
     return results.sort((a , b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
   }
 }
